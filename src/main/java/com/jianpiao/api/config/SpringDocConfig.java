47b54181ac660a26a9be350c0de76288443c716d
BaBy <614645323@qq.com>
2022-08-06 12:50:20 +0800

    feat: 引入springdoc 访问路径http://localhost:8080/swagger-ui/index.html

---
+++ src/main/java/com/jianpiao/api/config/SpringDocConfig.java
@@ -0,0 +1,29 @@
+package com.jianpiao.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "jianpiao-api",
+                description = "简票",
+                version = "1.0"
+        ),
+        security = @SecurityRequirement(name = "token")
+)
+@SecurityScheme(
+        name = "token",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SpringDocConfig {
+
+}
