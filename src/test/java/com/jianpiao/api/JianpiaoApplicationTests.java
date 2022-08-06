94209ca8c15e748be3aa956c3d944e7d07e3ee6d
BaBy <614645323@qq.com>
2022-08-06 11:36:12 +0800

    init: 本地测试环境下记得修改application.yml的数据库连接信息

---
+++ src/test/java/com/jianpiao/api/JianpiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianpiao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JianpiaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
