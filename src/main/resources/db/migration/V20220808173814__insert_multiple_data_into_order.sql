36d68d94e42543549d176c709f8a97c8c83e9f1f
Cherry <347497274@qq.com>
2022-08-08 17:39:04 +0800

    feat[#14]: insert record into table order

---
+++ src/main/resources/db/migration/V20220808173814__insert_multiple_data_into_order.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0001','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0002','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0003','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0004','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0005','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0006','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0007','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
