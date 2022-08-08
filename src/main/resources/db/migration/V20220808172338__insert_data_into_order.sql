117ae4f86a930abcdda467a183c84a557b98b370
Cherry <347497274@qq.com>
2022-08-08 17:24:57 +0800

    feat[#14]: insert record into table order

---
+++ src/main/resources/db/migration/V20220808172338__insert_data_into_order.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.tb_order (id,user_id,ticket) VALUES
+	 ('4028aa81827c9ca201827c9ce8fc0000','1','{"posterUrl":"https://s3.bmp.ovh/imgs/2022/08/08/a6e17a75dc134235.webp","totalPrice":24.520901997294676,"filmName":"爱在日落黄昏时"}');
