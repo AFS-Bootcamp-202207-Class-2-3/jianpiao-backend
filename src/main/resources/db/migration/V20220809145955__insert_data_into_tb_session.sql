5a8583f72e4ea06e005703baad7cb24d0fef4ab6
Cherry <347497274@qq.com>
2022-08-09 15:07:00 +0800

    feat[#22]:  add db.migration file

---
+++ src/main/resources/db/migration/V20220809145955__insert_data_into_tb_session.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.tb_session (id,film_id,hall_id,cinema_id,date,start_time,end_time,price,site) VALUES
+	 ('6', '1', '1','1', '2022-08-09', '19:00', '21:00', 25.00, '11111111111111111111111111111111111111111111111111111111111111111111111111111');
+INSERT INTO public.tb_session (id,film_id,hall_id,cinema_id,date,start_time,end_time,price,site) VALUES
+	 ('7', '1', '1','1', '2022-08-10', '19:00', '21:00', 25.00, '11111111111111111111111111111111111111111111111111111111111111111111111111111');
