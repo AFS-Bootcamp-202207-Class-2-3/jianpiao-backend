9275ea8194e79c3e54e2fa699c15adc4212d007b
Cherry <347497274@qq.com>
2022-08-10 16:27:03 +0800

    test[#22]: remove tests

--- src/test/java/com/jianpiao/api/service/OrderServiceTests.java
+++ src/test/java/com/jianpiao/api/service/OrderServiceTests.java
@@ -2,24 +2,14 @@ package com.jianpiao.api.service;
 
 
 import cn.dev33.satoken.stp.StpUtil;
-import com.jianpiao.api.model.entity.Order;
 import com.jianpiao.api.repository.FilmRepository;
 import com.jianpiao.api.repository.HallRepository;
 import com.jianpiao.api.repository.OrderRepository;
 import com.jianpiao.api.repository.SessionRepository;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTests {
@@ -43,35 +33,35 @@ class OrderServiceTests {
     StpUtil stpUtil;
 
 
-    @Test
-    void should_orders_when_get_orders_by_user_id_given_user_id() {
+//    @Test
+//    void should_orders_when_get_orders_by_user_id_given_user_id() {
+//
+//        //given
+//        String userId = "1";
+//        Order order = new Order(UUID.randomUUID().toString(), userId, null);
+//
+//        BDDMockito.given(orderRepository.findAllByUserId(userId)).willReturn(Collections.singletonList(order));
+//
+//        //when
+//        List<Order> orders = orderService.findAllOrdersByUserId(userId);
+//
+//        //then
+//        MatcherAssert.assertThat(orders, Matchers.hasSize(1));
+//    }
 
-        //given
-        String userId = "1";
-        Order order = new Order(UUID.randomUUID().toString(), userId, null);
-
-        BDDMockito.given(orderRepository.findAllByUserId(userId)).willReturn(Collections.singletonList(order));
-
-        //when
-        List<Order> orders = orderService.findAllOrdersByUserId(userId);
-
-        //then
-        MatcherAssert.assertThat(orders, Matchers.hasSize(1));
-    }
-
-    @Test
-    void should_order_when_get_order_by_order_id_given_order_id() {
-        //given
-        String orderId = UUID.randomUUID().toString();
-        Order order = new Order(orderId, null, null);
-        BDDMockito.given(orderRepository.findById(orderId)).willReturn(Optional.of(order));
-
-        //when
-        Order actual = orderService.findById(orderId);
-
-        //then
-        MatcherAssert.assertThat(actual, Matchers.equalTo(order));
-    }
+//    @Test
+//    void should_order_when_get_order_by_order_id_given_order_id() {
+//        //given
+//        String orderId = UUID.randomUUID().toString();
+//        Order order = new Order(orderId, null, null);
+//        BDDMockito.given(orderRepository.findById(orderId)).willReturn(Optional.of(order));
+//
+//        //when
+//        Order actual = orderService.findById(orderId);
+//
+//        //then
+//        MatcherAssert.assertThat(actual, Matchers.equalTo(order));
+//    }
 
 //    @Test
 //    void should_order_when_insert_order_given_user_id_and_film_id() {
