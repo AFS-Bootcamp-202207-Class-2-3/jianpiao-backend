4a3ba9dce6fcc60316c42b499814589812fe41df
Cherry <347497274@qq.com>
2022-08-10 17:32:20 +0800

    feat[#22]: alter code in tb_order

---
+++ src/main/resources/db/migration/V20220810173131__alter_code_in_tb_order.sql
@@ -0,0 +1,8 @@
+update tb_order set create_time = '2022-08-10', code = '1660060800001' where id = '4028aa81827c9ca201827c9ce8fc0000';
+update tb_order set create_time = '2022-08-10', code = '1660060800002' where id = '4028aa81827c9ca201827c9ce8fc0001';
+update tb_order set create_time = '2022-08-10', code = '1660060800003' where id = '4028aa81827c9ca201827c9ce8fc0002';
+update tb_order set create_time = '2022-08-10', code = '1660060800004' where id = '4028aa81827c9ca201827c9ce8fc0003';
+update tb_order set create_time = '2022-08-10', code = '1660060800005' where id = '4028aa81827c9ca201827c9ce8fc0004';
+update tb_order set create_time = '2022-08-10', code = '1660060800006' where id = '4028aa81827c9ca201827c9ce8fc0005';
+update tb_order set create_time = '2022-08-10', code = '1660060800007' where id = '4028aa81827c9ca201827c9ce8fc0006';
+update tb_order set create_time = '2022-08-10', code = '1660060800008' where id = '4028aa81827c9ca201827c9ce8fc0007';
