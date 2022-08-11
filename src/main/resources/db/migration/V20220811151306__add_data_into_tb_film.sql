ac04c985febaabdd7cc841c98bd6b208f6c85eeb
Cherry <347497274@qq.com>
2022-08-11 16:10:24 +0800

    feat[#45]: add flyway insert data into tb_film

---
+++ src/main/resources/db/migration/V20220811151306__add_data_into_tb_film.sql
@@ -0,0 +1,55 @@
+insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('4', '独行月球','人类为抵御小行星的撞击，拯救地球，在月球部署了月盾计划。陨石提前来袭，全员紧急撤离时，维修工独孤月（沈腾 饰）因为意外，错过了领队马蓝星（马丽 饰）的撤离通知，一个人落在了月球。不料月盾计划失败，独孤月成为了“宇宙最后的人类”，开始了他在月球上破罐子破摔的生活……',
+ '20220729', 122, '张吃鱼', '沈腾, 马丽, 常远, 李诚儒, 黄才伦, 辣目洋子', 'https://s1.328888.xyz/2022/08/11/6mK4I.jpg',4.5);
+
+insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('5', '猪猪侠大电影·海洋日记','超星特工猪猪侠在上次的任务中留下了后遗症，但他坚持独自潜入海底世界执行新任务。 在五彩缤纷的海底世界见识到各种各样的海底生物，结识了正在寻找哥哥的小豆芽，受小豆芽勇敢执着的精神所打动，猪猪侠决定帮助小豆芽，帮他一起寻找哥哥。 原来海洋世界发生了一件不为人知的大事，导致大量海洋“居民”失踪。这件事并不简单，背后隐藏着一个危及海洋世界和平的大危机。猪猪侠这次要如何克服困难，突破自我，解除海洋大危机呢？',
+ '20220806', 85, '钟彧', '陆双, 陈志荣', 'https://s1.328888.xyz/2022/08/11/6DJZP.jpg',4.2);
+
+ insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('6', '头文字D','十八岁的藤原拓海（周杰伦饰）每天驾着父亲藤原文太（黄秋生饰）残旧的汽车送递豆腐，却无意中练得一手出神入化的飘移技术。对汽车毫无兴趣的他被父亲怂恿参加山路赛，以平日送货的旧车越级挑战著名赛车队，赛果似乎早已定断。岂料藤原拓海竟在众人错愕之下胜出，一夜成名，更激发起他遗传的赛车欲，不断面对一连串惊险绝伦的挑战赛！',
+ '20050624', 108, '刘伟强、麦兆辉', '周杰伦, 铃木杏, 陈冠希, 黄秋生, 余文乐, 陈小春', 'https://s1.328888.xyz/2022/08/11/6D7qI.webp',4.8);
+
+
+insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('7', '环太平洋','该片主要讲述了人类为了抵抗怪兽的进攻，研制出了高大的机器战士与来犯怪兽进行对抗的故事。',
+ '20130712', 132, '吉尔莫·德尔·托罗', '查理·汉纳姆, 伊德瑞斯·艾尔巴, 菊地凛子, 芦田爱菜, 查理·戴', 'https://s1.328888.xyz/2022/08/11/6DqC7.webp',4.8);
+
+insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('8', '招魂','该片讲述了佩龙一家1971年搬进了一栋他们朝思暮想的房子里，房子座落在田园风光的罗德岛上，风景优美。然而一系列惊悚的事情接连发生，家里狗的离奇死亡，半夜孩子们被吓醒呼救。这栋房子变成他们的噩梦，十年后，每个人都变得魂不守舍，最终决定搬离罗德岛。',
+ '20130719', 112, '温子仁', '朗·里维斯顿, 维拉·法梅加, 帕特里克·威尔森', 'https://s1.328888.xyz/2022/08/11/6DRMw.webp',4.9);
+
+ insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('9', '霍元甲','影片从霍元甲儿时开始，一直讲述到八国联军入侵中国，霍元甲到上海挑战大力士成功，随后开办精武体操馆，但不久之后惨遭日本人下毒，英年早逝。该片于2006年1月25日上映。',
+ '20060125', 141, '于仁泰', '李连杰, 孙俪, 董勇', 'https://s1.328888.xyz/2022/08/11/6DvQn.webp',4.6);
+
+
+ insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('10', '肖申克的救赎','一场谋杀案使银行家安迪（蒂姆•罗宾斯 Tim Robbins 饰）蒙冤入狱，谋杀妻子及其情人的指控将囚禁他终生。在肖申克监狱的首次现身就让监狱“大哥”瑞德（摩根•弗里曼 Morgan Freeman 饰）对他另眼相看。瑞德帮助他搞到一把石锤和一幅女明星海报，两人渐成患难 之交。很快，安迪在监狱里大显其才，担当监狱图书管理员，并利用自己的金融知识帮助监狱官避税，引起了典狱长的注意，被招致麾下帮助典狱长洗黑钱。',
+ '19940910', 142, '弗兰克·德拉邦特', ' 蒂姆·罗宾斯, 摩根·弗里曼, 鲍勃·冈顿, 威廉姆·赛德', 'https://s1.328888.xyz/2022/08/11/6Fdn6.webp', 5.0);
+
+ insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('11', '山海经之再见怪兽','昆仑之虚，掩于山海之中，滋养了无数生灵。这里的昆仑医馆医术精湛，但却有一种病千百年来无药可医——黑灵。医师白泽致力对抗黑灵，却自以为是毁了整个大岛，被逐出医馆。 七年后，白泽的医术突飞猛进却受人白眼无法行医，想借医馆选举一雪前耻，却在阴差阳错之下，与自卑的麒麟男孩小羿一起踏上了荒芜的大岛，进行一场救赎之旅。',
+ '20220813', 99, '黄健明', '刘琮, 艾一艾', 'https://s1.328888.xyz/2022/08/11/6Ommr.jpg', null);
+
+  insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('12', '人间世','电影《人间世》由陶涛、张琪、秦博、范士广四位导演共同执导，陶涛，张琪联合监制，秦博、范士广任总策划。影片选取两位身患绝症的病人，将目光聚焦于她们的家庭，记录下她们人生最后的时光，书写了一首关乎爱的生命诗篇。',
+ '20220819', 90, '陶涛, 张琪, 秦博, 范士广, 张琪', '许烈英,王思蓉,刘子涵', 'https://s1.328888.xyz/2022/08/11/6OFym.jpg', null);
+
+  insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('13', '乡土','影片取材于河北省石家庄市元氏县槐阳镇铁屯村，以该村已故党委书记闫胜联为原型，艺术化再现了闫胜联在深入实施乡村振兴，加快城乡融合发展过程中，一手抓产业振兴、一手为老百姓分忧解难，全面赢得民心的感人故事。',
+ '20220819', 112, '周德新', '何政军,李健,来喜,翟小兴', 'https://s1.328888.xyz/2022/08/11/6ObDh.jpg',null);
+
+  insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('14', '760号犯人','影片改编自Mohamedou Ould Slahi的回忆录《关塔那摩日记》，讲述Slahi(塔哈饰)被美国政府俘虏，在毫无指控与判决的情况下被关进了监狱，他失去了希望。Slahi寻得辩护律师南希·霍兰德(福斯特饰)与助手特里·邓肯(伍德蕾饰)的帮助，他们在面对无数障碍的情况下，一同追求正义。',
+ '20220823', 128, '凯文·麦克唐纳', '朱迪·福斯特,塔哈·拉希姆,谢琳·伍德蕾,本尼迪克特·康伯巴奇,扎克瑞·莱维', 'https://s1.328888.xyz/2022/08/11/6Okbp.jpg',null);
+
+
+ insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('15', '阿甘正传','阿甘（汤姆·汉克斯 饰）于二战结束后不久出生在美国南方阿拉巴马州一个闭塞的小镇，他先天弱智，智商只有75，然而他的妈妈是一个性格坚强的女性，她常常鼓励阿甘“傻人有傻福”，要他自强不息。阿甘像普通孩子一样上学，并且认识了一生的朋友和至爱珍妮（罗宾·莱特·潘 饰），在珍妮 和妈妈的爱护下，阿甘凭着上帝赐予的“飞毛腿”开始了一生不停的奔跑。',
+ '19940623', 142, '罗伯特·泽米吉斯', '艾瑞克·罗斯, 温斯顿·格鲁姆', 'https://s1.328888.xyz/2022/08/11/6OcI5.webp',4.9);
+
+  insert into "tb_film" ("id", "film_name", "introduction", "released_time",  "duration", "director", "leading_actor", "poster_url", "score")
+ values ('16', '千与千寻 千と千尋の神隠し ','千寻和爸爸妈妈一同驱车前往新家，在郊外的小路上不慎进入了神秘的隧道——他们去到了另外一个诡异世界—一个中世纪的小镇。远处飘来食物的香味，爸爸妈妈大快朵颐，孰料之后变成了猪！这时小镇上渐渐来了许多样子古怪、半透明的人。',
+ '20190621', 125, '宫崎骏', '柊瑠美, 入野自由, 夏木真理, 菅原文太, 中村彰男', 'https://s1.328888.xyz/2022/08/11/6OIDy.webp',4.8);
+
