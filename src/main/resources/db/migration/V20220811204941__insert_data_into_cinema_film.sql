ce4019981fba2392c5dac2221eee3cfe23d039d7
yysauce <347497274@qq.com>
2022-08-11 20:52:33 +0800

    feat: add flyway insert data into cinema_film

---
+++ src/main/resources/db/migration/V20220811204941__insert_data_into_cinema_film.sql
@@ -0,0 +1,18 @@
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('10', '1', '4', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('11', '1', '5', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('12', '1', '6', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('13', '1', '7', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('14', '1', '8', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('15', '1', '9', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('16', '1', '10', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('17', '1', '15', 'showing');
+insert into public.cinema_film (id, cinema_id, film_id, status)
+values ('18', '1', '16', 'showing');
